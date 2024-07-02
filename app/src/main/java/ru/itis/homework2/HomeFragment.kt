4b20c0d47db3402d12650e0e70f553e99673bd1c
Artfal11 <ilshat3092005@gmail.com>
2024-07-02 07:19:24 +0300

    second-commit-final

--- app/src/main/java/ru/itis/homework2/HomeFragment.kt
+++ app/src/main/java/ru/itis/homework2/HomeFragment.kt
@@ -15,19 +15,6 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_home, container, false)
-
-        val bottomNavigationView = view.findViewById<BottomNavigationView>(R.id.bottom_navigation)
-        bottomNavigationView.setOnNavigationItemSelectedListener { item ->
-            when (item.itemId) {
-                R.id.homeFragment -> findNavController().navigate(R.id.homeFragment)
-                R.id.searchFragment -> findNavController().navigate(R.id.searchFragment)
-                R.id.settingsFragment -> findNavController().navigate(R.id.settingsFragment)
-                R.id.notificationsFragment -> findNavController().navigate(R.id.notificationsFragment)
-                R.id.profileFragment -> findNavController().navigate(R.id.profileFragment)
-            }
-            true
-        }
-
         return view
     }
 }
--- app/src/main/java/ru/itis/homework2/MainActivity.kt
+++ app/src/main/java/ru/itis/homework2/MainActivity.kt
@@ -11,12 +11,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val navHostFragment = supportFragmentManager
-            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        val navController = navHostFragment.navController
-
-        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
-        bottomNavigationView.setupWithNavController(navController)
+        val controller = (supportFragmentManager.findFragmentById(R.id.container) as NavHostFragment)
+            .navController
+        findViewById<BottomNavigationView>(R.id.bottomNavigation).apply {
+            setupWithNavController(controller)
+        }
     }
 }
